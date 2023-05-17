70bedf0d6253bf6e079ffa47eb4b4285ffdc6041
Nishchal <nishchal.jatwani@gmail.com>
2023-05-16 21:49:48 -0400

    chore:links add

--- src/components/RightInfoContainer.js
+++ src/components/RightInfoContainer.js
@@ -1,3 +1,7 @@
+import GitHub from "../icons/GitHub";
+import Email from "../icons/Email";
+import Linkedin from "../icons/LinkedIn";
+
 export default function RightInfoContainer() {
     return (
         <div className="rightBox-text-wrapper">
@@ -21,17 +25,24 @@ export default function RightInfoContainer() {
             web development, and Python for scripting.
           </div>
 
-          <div className = "links-sites">
+          {/*Links*/}
+          <div className = "link-sites" >
             
+            {/*GitHub*/}
+            <div className = "link-github">
+              <a href = "https://github.com/nish00" style={{color:"gray"}}><GitHub/></a>
+            </div>
+
+            {/*LinkedIn*/}
+            <div className = "link-linkedin">
+              <a href = "https://linkedin.com/in/nishchaljatwani" style={{color:"gray"}}><Linkedin/></a>
+            </div>
+
+            {/*Email*/}
+            <div className = "link-email">
+             <a href = "mailto:nishchal.jatwani@gmail.com" style={{color:"gray"}}><Email/></a>
+            </div>
           </div>
-
-
-
-
-
-
-
-
         </div>
     );
   }
